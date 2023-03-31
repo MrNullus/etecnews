51dd3bfe8104deaf087e7220eb481358466144a5
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-03-30 21:59:08 -0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function abrirCadastro() {
+    public function abrirCadastro()
+    {
         return view('admin.formusuarios');
     }
 
-    public function abrirConsulta() {
-        return view('admin.consusuarios');
+    public function cadastrar(Request $req)
+    {
+        $user = new User([
+            'name'     => $req->name,
+            'email'    => $req->email,
+            'password' => $req->password,
+            'level'    => $req->level
+        ]);
+
+        $user->save();
+
+        return redirect('/admin/usuarios')
+            ->with('message', 'Cadastrado com sucesso!');
+    }
+
+    public function abrirUsuarios()
+    {
+        $dadosUser = User::all();
+
+        return view('admin.consusuarios', [
+            'dadosUser' => $dadosUser,
+        ]);
     }
 }
